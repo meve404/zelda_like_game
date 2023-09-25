57826ec1e1c1f1dca606c0cededc2c480ac61a4b
meve404 <procontact325@gmail.com>
2023-09-25 18:11:24 -0500

    main setup and levels

---
+++ setup/level.py
@@ -0,0 +1,12 @@
+import pygame
+
+class level:
+    def __init__(self):
+
+        # Sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+    
+    def run(self):
+        # update and draw the game
+        pass
--- setup/main.py
+++ setup/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from debug import debug
 
 class Game:
 	def __init__(self):
@@ -7,20 +8,21 @@ class Game:
 		# general setup
 		pygame.init()
 		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
+		pygame.display.set_caption('Zelda Like Game') # Sets the screen caption
 		self.clock = pygame.time.Clock()
 	
 	def run(self):
 		while True:
+			# Event loop
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
 
-			self.screen.fill('black')
-			pygame.display.update()
-			self.clock.tick(FPS)
+			self.screen.fill('black') # Fills the screen with black
+			pygame.display.update() # Updates the screen
+			self.clock.tick(FPS) # Controls the frame rate
 
 if __name__ == '__main__':
 	game = Game()
-	game.run()	
+	game.run()
