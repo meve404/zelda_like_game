f0b5d74f31e3f33e2ccc714ec7987f47fa720785
meve404 <procontact325@gmail.com>
2023-09-26 16:42:57 -0500

    Assign x and p to sprites

--- setup/level.py
+++ setup/level.py
@@ -1,6 +1,8 @@
 import pygame
 from debug import debug
-from settings import WORLD_MAP
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -12,13 +14,20 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
 
-        # Sprites setup
+        # Sprite setup
         self.create_map()
     
     def create_map(self):
-        for row in WORLD_MAP:
-            print(row)
+        # To check the index and value of each list
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x': # Assigns a sprite to 'x'
+                    Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
+                elif col == 'p': # Assigns a sprite to 'p'
+                    Player((x,y), [self.visible_sprites])
 
     def run(self):
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
