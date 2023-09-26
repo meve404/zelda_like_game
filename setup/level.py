31bd707c6f37d9394be582767699b5657b9fcf1e
meve404 <procontact325@gmail.com>
2023-09-26 17:57:12 -0500

    setting up the player

--- setup/level.py
+++ setup/level.py
@@ -1,8 +1,8 @@
 import pygame
-from debug import debug
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -11,7 +11,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # Sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
         # Sprite setup
@@ -26,8 +26,28 @@ class Level:
                 if col == 'x': # Assigns a sprite to 'x'
                     Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
                 elif col == 'p': # Assigns a sprite to 'p'
-                    Player((x,y), [self.visible_sprites])
+                    self.player = Player((x,y), [self.visible_sprites], self.obstacles_sprites)
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # General setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2() # Draws the sprites on a different spot
+
+    def custom_draw(self, player):
+        # Getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_postition =  sprite.rect.topleft - self.offset # Creates a camera by drawing the sprites where the player is
+            self.display_surface.blit(sprite.image, offset_postition)
